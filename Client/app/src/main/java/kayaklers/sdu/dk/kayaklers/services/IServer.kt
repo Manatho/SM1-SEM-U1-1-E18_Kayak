f114406fa83290a4f57a790704cddbbc24e6db68
Unknown <mathies-h@hotmail.com>
2018-09-21 13:12:03 +0200

    IServer interface for the client

---
+++ Client/app/src/main/java/kayaklers/sdu/dk/kayaklers/services/IServer.kt
@@ -0,0 +1,14 @@
+package kayaklers.sdu.dk.kayaklers.services
+
+import kayaklers.sdu.dk.kayaklers.data.Log
+
+interface IServer {
+    fun getLogs() : List<Log>
+    fun addLog() : Log
+    fun addLogs() : List<Log>
+
+    fun getTotalTravelTime() : Long
+    fun getTotalTravelDistance() : Double
+
+
+}
