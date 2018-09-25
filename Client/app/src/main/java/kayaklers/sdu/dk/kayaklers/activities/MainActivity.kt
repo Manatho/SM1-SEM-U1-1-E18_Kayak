5888d4349b9b639903931c115146abafdb8fee11
Unknown <mads1108@hotmail.dk>
2018-09-25 09:05:37 +0200

    Activities

--- Client/app/src/main/java/kayaklers/sdu/dk/kayaklers/activities/MainActivity.kt
+++ Client/app/src/main/java/kayaklers/sdu/dk/kayaklers/activities/MainActivity.kt
@@ -1,13 +1,54 @@
 package kayaklers.sdu.dk.kayaklers.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
 import kayaklers.sdu.dk.kayaklers.R
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var btn_se_logs: Button
+    private lateinit var btn_ny_log: Button
+    private lateinit var btn_kort: Button
+    private lateinit var btn_vejr: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_se_logs = findViewById(R.id.se_logs) as Button
+        btn_ny_log = findViewById(R.id.ny_log) as Button
+        btn_kort = findViewById(R.id.kort) as Button
+        btn_vejr = findViewById(R.id.vejr) as Button
+
+        btn_se_logs.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(view: View) {
+                val intent = Intent(this@MainActivity, GetLogsActivity::class.java);
+                startActivity(intent)
+            }
+        })
+
+        btn_ny_log.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(view: View) {
+                val intent = Intent(this@MainActivity, NewLogActivity::class.java);
+                startActivity(intent)
+            }
+        })
+
+        btn_kort.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(view: View) {
+                val intent = Intent(this@MainActivity, MapActivity::class.java);
+                startActivity(intent)
+            }
+        })
+
+        btn_vejr.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(view: View) {
+                val intent = Intent(this@MainActivity, WeatherActivity::class.java);
+                startActivity(intent)
+            }
+        })
     }
 }
---
+++ Client/app/src/main/java/kayaklers/sdu/dk/kayaklers/activities/MapActivity.kt
@@ -0,0 +1,72 @@
+package kayaklers.sdu.dk.kayaklers.activities
+
+import android.content.pm.PackageManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import kayaklers.sdu.dk.kayaklers.Manifest
+import kayaklers.sdu.dk.kayaklers.R
+import kotlinx.android.synthetic.main.activity_map.*
+
+class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+
+
+    private lateinit var map: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private val LOCATION_PERMISSION_REQUEST_CODE = 9001
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        map.getUiSettings().setZoomControlsEnabled(true)
+        map.setOnMarkerClickListener(this)
+        setUpMap()
+
+
+    }
+
+    override fun onMarkerClick(p0: Marker): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+        map.isMyLocationEnabled = true
+
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+            if (location != null) {
+                lastLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+            }
+        }
+
+    }
+
+
+
+}
