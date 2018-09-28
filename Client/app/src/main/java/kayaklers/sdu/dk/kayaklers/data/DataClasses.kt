3820999813637e75148f4de07db61f4bbd6b8a3d
Unknown <mathies-h@hotmail.com>
2018-09-28 09:16:34 +0200

    Updated GPSPoint to parse from and to Location

--- Client/app/src/main/java/kayaklers/sdu/dk/kayaklers/data/DataClasses.kt
+++ Client/app/src/main/java/kayaklers/sdu/dk/kayaklers/data/DataClasses.kt
@@ -1,4 +1,22 @@
 package kayaklers.sdu.dk.kayaklers.data
 
+import android.location.Location
+
 data class Log (val id: Long, val duration: Long, val distance : Double, val valid : Boolean, val points : Int, val gpsPoints : List<GPSPoint>)
-data class GPSPoint(val latitude : Long, val longitude : Long, val altitude : Long)
+
+data class GPSPoint(var latitude : Double, var longitude : Double, var altitude : Double){
+    fun of(location : Location) {
+        latitude = location.latitude
+        longitude = location.longitude
+        altitude = location.altitude
+    }
+
+
+    fun toLocation() : Location{
+        var location = Location("")
+        location.latitude = latitude
+        location.longitude = longitude
+        location.altitude = altitude
+        return location;
+    }
+}
