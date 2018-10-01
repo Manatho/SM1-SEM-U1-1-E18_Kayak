9fe5fa496e834b6d04c9da3968a8eea9ccfc415e
Holst <moviemastersdk@gmail.com>
2018-10-01 13:59:37 +0200

    LocationTracking | KayakLog branding | Persistent Notification

--- Client/app/src/main/java/kayaklers/sdu/dk/kayaklers/activities/NewLogActivity.kt
+++ Client/app/src/main/java/kayaklers/sdu/dk/kayaklers/activities/NewLogActivity.kt
@@ -3,20 +3,25 @@ package kayaklers.sdu.dk.kayaklers.activities
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kayaklers.sdu.dk.kayaklers.R
 import kayaklers.sdu.dk.kayaklers.services.LocationMonitoringService
+import kotlinx.android.synthetic.main.activity_new_log.*
 
 class NewLogActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_log)
+
+        this.btnStartLog.setOnClickListener {startNewLog()}
+
     }
 
-    fun createNewLog(){
+    fun startNewLog(){
         //Start LocationMonitoring Service
-        val locationService = Intent(this@NewLogActivity, LocationMonitoringService::class.java)
-        startService(locationService)
+        val locationTracker = Intent(this@NewLogActivity, LocationMonitoringService::class.java)
+        startService(locationTracker)
     }
 
     //Opens up map fragment with and passes in route data to see current route
