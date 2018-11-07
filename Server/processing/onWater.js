8459053637aa9b1f86ef619c20c48ecc57925008
Unknown <mathies-h@hotmail.com>
2018-11-07 18:45:37 +0100

    Moved coordinate assignment out into the server

--- Server/processing/onWater.js
+++ Server/processing/onWater.js
@@ -26,62 +26,70 @@ function getImageAtCoord(coord) {
 
 module.exports = {
 	async process(points) {
-		const pixels = await getImageAtCoord({ latitude: 55.3497, longitude: 10.361572 });
+		let text = "";
+
 		//const pixels = await getImageAtCoord({ latitude: 55.3496028, longitude: 10.361572 });
 
-		let { shape } = pixels;
-		let width = shape[0];
-		let height = shape[1];
+		for (let i = 0; i < points.length; i++) {
+			const point = points[i];
 
-		let yStartIndex = Math.round(height * 0.25),
-			yEndIndex = Math.round(height * 0.75);
-		let centercoords = { x: width / 2, y: (yEndIndex - yStartIndex) / 2 };
-		let watercoords = [];
-		let pixelCount = (yEndIndex - yStartIndex) * width;
+			let pixels = await getImageAtCoord({ latitude: point.latitude, longitude: point.longitude });
+			let { shape } = pixels;
+			let width = shape[0];
+			let height = shape[1];
 
-		let sum = 0;
-		for (let x = 0; x < width; x++) {
-			for (let y = yStartIndex; y < yEndIndex; y++) {
-				let value = pixels.get(x, y, 3);
-				let yoff = y - yStartIndex;
+			let yStartIndex = Math.round(height * 0.25),
+				yEndIndex = Math.round(height * 0.75);
+			let centercoords = { x: width / 2, y: (yEndIndex - yStartIndex) / 2 };
+			let watercoords = [];
+			let pixelCount = (yEndIndex - yStartIndex) * width;
 
-				if (value != 0) {
-					sum++;
-					if (watercoords[yoff] == null) {
-						watercoords[yoff] = [];
+			let sum = 0;
+			for (let x = 0; x < width; x++) {
+				for (let y = yStartIndex; y < yEndIndex; y++) {
+					let value = pixels.get(x, y, 3);
+					let yoff = y - yStartIndex;
+
+					if (value != 0) {
+						sum++;
+						if (watercoords[yoff] == null) {
+							watercoords[yoff] = [];
+						}
+						watercoords[yoff][x] = 1;
 					}
-					watercoords[yoff][x] = 1;
 				}
 			}
-		}
-		let waterPercentage = sum / pixelCount;
+			let waterPercentage = sum / pixelCount;
 
-		if (sum != 0) {
-			let result = closestWaterPixel(watercoords, centercoords, width, yEndIndex - yStartIndex);
-			drawPng(width, yEndIndex - yStartIndex, watercoords, result);
-			console.log("center: ", centercoords, "Nearst water: ", result);
-		} else {
-			console.log("Couldn't find any water");
-		}
+			if (sum != 0) {
+				let result = closestWaterPixel(watercoords, centercoords, width, yEndIndex - yStartIndex);
+				text += drawPng(width, yEndIndex - yStartIndex, watercoords, result);
+				text += `center:  ${JSON.stringify(centercoords)} Nearst water: ${JSON.stringify(result)}\n`;
+			} else {
+				text += "Couldn't find any water\n";
+			}
 
-		console.log(`Water percentage = ${(waterPercentage * 100).toFixed(2)}%`);
+			text += `Water percentage = ${(waterPercentage * 100).toFixed(2)}%\n`;
+		}
+		return text;
 	}
 };
 
 function drawPng(width, height, waterpixels, result) {
+	let text = "";
 	for (let y = 0; y < height; y++) {
-		let text = "";
 		for (let x = 0; x < width; x++) {
 			if (x == (width / 2) >> 0 && y == (height / 2) >> 0) {
-				text += " C ";
+				text += "_C_";
 			} else if (x == result.x && y == result.y) {
-				text += "   ";
+				text += "___";
 			} else if (waterpixels[y] != null) {
 				text += waterpixels[y][x] ? "▒▒▒" : "███";
 			} else {
 				text += "███";
 			}
 		}
-		console.log(text);
+		text += "\n";
 	}
+	return text;
 }
--- Server/server.js
+++ Server/server.js
@@ -9,21 +9,28 @@ const port = 4000;
 var app = express();
 
 app.get("/test", async function(req, res) {
-  const test = onWaterProcessor.process([]);
-  console.log(test)
-  res.send('testing')
+	const test = await onWaterProcessor.process([
+		{ latitude: 55.3496028, longitude: 10.361572 },
+		{ latitude: 55.3496228, longitude: 10.361672 },
+		{ latitude: 55.3496228, longitude: 10.361752 },
+		{ latitude: 55.3496228, longitude: 10.361832 },
+		{ latitude: 55.3496228, longitude: 10.361912 }
+	]);
+	console.log(test);
+	res.send(`<div style="
+  height:10000px; 
+  font-family: Monospace;
+  letter-spacing: -1px;
+  width: 900px;
+  ">${test}</div>`);
 });
 
 app.use(
-  "/graphql",
-  express_graphql({
-    schema: schema,
-    graphiql: true
-  })
+	"/graphql",
+	express_graphql({
+		schema: schema,
+		graphiql: true
+	})
 );
 
-app.listen(port, () =>
-  console.log(
-    "Express GraphQL Server Now Running On https://kayaklers.localtunnel.me/graphql"
-  )
-);
+app.listen(port, () => console.log("Express GraphQL Server Now Running On https://kayaklers.localtunnel.me/graphql"));
